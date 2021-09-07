f8dfaa593722026f53685bd1a03cf4464dafbddb
9pedobear <kayratsagynbekov@gmail.com>
2021-09-07 14:50:45 +0600

    Генераторы и событийный цикл Round Robin

--- asyncio/original.py
+++ asyncio/original.py
@@ -1,52 +1,48 @@
-import socket
-import selectors
+from time import time
 
 
-selector = selectors.DefaultSelector()
+# python3 -i original.py
+# gen
+# g
+# next(g)
+# next('ol')
 
 
-def server():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind(('localhost', 5001))
-    server_socket.listen()
-
-    selector.register(fileobj=server_socket, events=selectors.EVENT_READ, data=accept_connection)
-
-
-def accept_connection(server_socket):
-    client_socket, addr = server_socket.accept()
-    print('Connection from', addr)
-
-    selector.register(fileobj=client_socket, events=selectors.EVENT_READ, data=send_message)
-
-
-def send_message(client_socket):
-
-    request = client_socket.recv(4096)
-
-    if request:
-        response = 'Hello world\n'.encode()
-        client_socket.send(response)
-    else:
-        selector.unregister(client_socket)
-        client_socket.close()
-
-
-def event_loop():
+def gen_filename():
     while True:
+        pattern = 'file-{}.jpeg'
+        t = int(time() * 1000)
 
-        events = selector.select()   # (key, events)
+        yield pattern.format(str(t))
 
-        for key, _ in events:
-            callback = key.data
-            callback(key.fileobj)
-
+        sum = 234 + 234
+        print(sum)
 
 
-if __name__ == '__main__':
-    server()
-    event_loop()
-    
-# 1 terminal python3 original.py
-# 2 terminal nc localhost 5001
+# g = gen_filename()
+
+
+def gen1(s):
+    for i in s:
+        yield i
+
+
+
+def gen2(n):
+    for i in range(n):
+        yield i
+
+g1 = gen1('oleg')
+g2 = gen2(4)
+
+tasks = [g1, g2]
+
+while tasks:
+    task = tasks.pop(0)
+
+    try:
+        i = next(task)
+        print(i)
+        tasks.append(task)
+    except StopIteration:
+        pass
