333649df32c13172ba2e22e8f1b1ac31433dd8ed
9pedobear <kayratsagynbekov@gmail.com>
2021-09-07 16:10:06 +0600

    Asyncio, async/await

--- asyncio/original.py
+++ asyncio/original.py
@@ -1,46 +1,68 @@
-# python3 -i  original.py
-# sg = subgen()
-# g = delegator(sg)
-# g.send('OK')
-# g.send(123)
-# g.throw(BlaBlaException)
+# pip (pip3) install aiohttp
 
 
+# План:
+# 1. Asyncio фреймворк для создания событийных циклов
+# 2. Пример простой асинхронной программы времён Python 3.4
+# 3. Синтаксис Async/await на замену @asyncio.coroutine и yield from
+# 4. Пример асинхронного скачивания файлов
 
 
-def coroutine(func):
-    def inner(*args, **kwargs):
-        g = func(*args, **kwargs)
-        g.send(None)
-        return g
-    return inner
+# Event Loop:
+#     coroutine > Task (Future)
+
+import asyncio
+from time import time
 
 
-class BlaBlaException(Exception):
-    pass
-
-
-
-def subgen():
+async def print_nums():
+    num = 1
     while True:
-        try:
-            message = yield
-        except StopIteration:
-            # print('Ku-ku!!!')
-            break
-        else:
-            print('........', message)
-
-    return 'Returned from subgen()'
+        print(num)
+        num += 1
+        await asyncio.sleep(0.1)
 
 
-@coroutine
-def delegator(g):
-    # while True:
-    #     try:
-    #         data = yield
-    #         g.send(data)
-    #     except BlaBlaException as e:
-    #         g.throw(e)
-    result = yield from g
-    print(result)
+async def print_time():
+    count = 0
+    while True:
+        if count % 3 == 0:
+            print("{} seconds have passed".format(count))
+        count += 1
+        await asyncio.sleep(1)
+
+
+async def main():
+    task1 = asyncio.create_task(print_nums())
+    task2 = asyncio.create_task(print_time())
+
+    await asyncio.gather(task1, task2)
+
+
+if __name__ == '__main__':
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(main())
+    # loop.close()
+    asyncio.run(main())
+    
+    
+# python3
+# import asyncio
+# @asyncio.corotine
+# def a():
+#    return 'ok'
+# 
+# import inspect
+# inspect.isgeneratorfunction(a)
+# g = a()
+# next(g)
+# def a():
+#	num = 1
+# 	while True:
+#		print(num)
+#		num += 1
+# a()
+
+
+# python3 original.py
+
